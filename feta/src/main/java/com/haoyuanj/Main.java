baab693add3f73a1e5b3a2e3980dbafae6abd754
HaoYuan Jiang <jhypewpew@gmail.com>
2025-07-14 20:27:16 +0800

    implement CLI

--- feta/src/main/java/com/haoyuanj/Main.java
+++ feta/src/main/java/com/haoyuanj/Main.java
@@ -1,11 +1,55 @@
 package com.haoyuanj;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.apache.commons.cli.*;
+
+import java.io.*;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+  public static void main(String[] args) {
+    Options options = new Options();
+    options.addOption(
+        Option.builder("f")
+            .longOpt("file")
+            .hasArg(true)
+            .numberOfArgs(1)
+            .required(true)
+            .desc("Path of input NEM12 file.")
+            .build());
+    options.addOption(
+        Option.builder("o")
+            .longOpt("output")
+            .hasArg(true)
+            .numberOfArgs(1)
+            .required(true)
+            .desc("Path of output file containing SQL insert queries.")
+            .build());
+
+    CommandLineParser parser = new DefaultParser();
+    CommandLine cmd = null;
+    try {
+      cmd = parser.parse(options, args);
+    } catch (ParseException pe) {
+      System.out.println("Error parsing command-line arguments: " + pe.getMessage());
+      System.exit(1);
     }
+
+    File inputFile = new File(cmd.getOptionValue("f"));
+    List<IntervalMeteringData> data = null;
+    try (InputStream s = new FileInputStream(inputFile)) {
+      data = IntervalMeteringDataReader.read(s);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+      System.exit(1);
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+
+    for (IntervalMeteringData d : data) {
+      System.out.println(d);
+    }
+
+    // TODO: produce output
+  }
 }
